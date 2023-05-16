88dfac670a03d543a5d35fceed823e9b388f776b
Feibrin <129807664+Feibrin@users.noreply.github.com>
2023-05-15 22:01:49 -0300

    Update alunos_media.cpp

--- alunos_media.cpp
+++ alunos_media.cpp
@@ -3,28 +3,37 @@ using namespace std;
 
 int main(){
     
+    // definindo o tamanho do vetor
     const int n = 6;
+    // criando o vetor pessoas com os dados
     int pessoas[n] = {35, 4, 22, 20, 36, 30};
+    // variável para guardar a soma das pessoas em todas as salas
     int soma = 0;
     
+    // loop para somar as pessoas em todas as salas
     for (int i = 0; i < n; i++) {
         soma += pessoas[i];
     }
     
+    // cálculo da média de pessoas por sala
     double media = soma / n;
     
+    // imprimindo a média
     cout << "Media de pessoas por sala: " << media << endl;
+    // imprimindo as salas com número de pessoas acima da média
     cout << "Salas com numero de pessoas acima da media: ";
     
+    // loop para verificar as salas com número de pessoas acima da média
     for (int i = 0; i < n; i++) {
+        // se o número de pessoas na sala i é maior do que a média
         if (pessoas[i] > media) {
+            // imprime o número da sala
             cout << i+1 << ";";
         }
     }
     
+    // pula uma linha para melhorar a visualização
     cout << endl;
     
     return 0;
 }
-
-//a utilização do array foi útil pois ele armazena uma sequência de elementos do mesmo tipo em uma única variável, no caso, pra armazenar a quantidade de pessoas por sala//
